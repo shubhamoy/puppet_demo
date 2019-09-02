03797dc03684e1cf63e2471c2f9c4a4aa3297a62
Shubhamoy <me@shubhamoy.com>
2019-09-02 10:37:39 +0530

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -6,7 +6,7 @@ class minecraft {
   }
   file {'/opt/minecraft/server.jar':
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar'
+    source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
     before => Service['minecraft'],
   }
   package {'java':

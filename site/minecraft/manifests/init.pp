602e8966cd45ecc3d117df88838718c490ca5274
Shubhamoy <me@shubhamoy.com>
2019-09-02 10:35:57 +0530

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,6 +7,7 @@ class minecraft {
   file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar'
+    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
@@ -20,7 +21,8 @@ class minecraft {
     source => 'puppet:///modules/minecraft/minecraft.service'
   }
   service {'minecraft':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => [Package['java'], File['/opt/minecraft/eula.txt'], File['/etc/systemd/system/minecraft.service']], 
   }
 }
